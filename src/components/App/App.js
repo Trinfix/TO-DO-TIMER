68b954c800121365ff1fe40f83872ade08d20687
Mati <matix1100@o2.pl>
2020-09-20 15:12:38 +0200

    prepared folders and files

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+
+  //routing
+  return (
+    <div className="App">
+      <h1>Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
